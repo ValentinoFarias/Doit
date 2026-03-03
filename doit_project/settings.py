c437e206438189464369d08d2660100bb40a538e
GiovanniDevs <damico.giovanni.90@gmail.com>
2026-03-03 16:22:46 +0000

    fix index.html extends tag

--- doit_project/settings.py
+++ doit_project/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fr1d^v1(#52g3=(t(1hiuh#e1e*^^6lt9qn*lr!6w=#4czxv(v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
